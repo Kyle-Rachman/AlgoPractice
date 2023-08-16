2bb7bb961c17ed0edc7b31f247666cd01be379e2
Kyle Rachman <127147261+Kyle-Rachman@users.noreply.github.com>
2023-08-15 23:14:08 -0700

    Time: 71 ms (26.25%), Space: 45 MB (52.50%) - LeetHub

--- 2639-find-the-width-of-columns-of-a-grid/2639-find-the-width-of-columns-of-a-grid.js
+++ 2639-find-the-width-of-columns-of-a-grid/2639-find-the-width-of-columns-of-a-grid.js
@@ -11,15 +11,11 @@ var findColumnWidth = function(grid) {
             return num.toString().length;
         }
     };
-    let transposedGrid = grid[0].map((col, i) => grid.map(row => row[i]))
     
-    for (let i = 0; i < transposedGrid.length; i++) {
-        let maxLen = 0;
-        for (let j = 0; j < transposedGrid[i].length; j++) {
-            maxLen = Math.max(lengthCheck(transposedGrid[i][j]),maxLen);
-            console.log(maxLen)
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid[i].length; j++) {
+            ans[j] = Math.max(lengthCheck(grid[i][j]),ans[j]);
         };
-        ans[i] = maxLen;
     };
     
     return ans;
