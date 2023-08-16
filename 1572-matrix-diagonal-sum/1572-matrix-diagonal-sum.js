6f80318e7720567408d72b02e222fdf2b22bcf83
Kyle Rachman <127147261+Kyle-Rachman@users.noreply.github.com>
2023-08-16 11:07:07 -0700

    Time: 45 ms (96.63%), Space: 44.1 MB (29.97%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} mat
+ * @return {number}
+ */
+var diagonalSum = function(mat) {
+    let sum = 0;
+    const n = mat.length;
+    for (let i = 0; i < n; i++) {
+        sum += mat[i][i] + mat[i][n-1-i]
+    };
+    
+    if (n%2 != 0) {
+        sum -= mat[(n-1)/2][(n-1)/2]
+    };
+    
+    return sum
+};
