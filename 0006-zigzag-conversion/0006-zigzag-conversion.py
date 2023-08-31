9dc39a5c4c10b1c4fe0a6366a4039ddf60762f47
Kyle Rachman <127147261+Kyle-Rachman@users.noreply.github.com>
2023-08-31 11:10:26 -0700

    Time: 36 ms (71.44%), Space: 13.4 MB (72.75%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -5,34 +5,19 @@ class Solution(object):
         :type numRows: int
         :rtype: str
         """
-        j = 0
         if (len(s) <= numRows) or (numRows == 1):
             return s
-        numCols = (numRows - 1) * (len(s) // (2*(numRows - 1))) + numRows - 1
-        matrixOutput = []
         
-        for i in range(numCols):
-            if (i % (numRows-1)) == 0:
-                matrixOutput.append([char for char in s[j*(numRows-1):j*(numRows-1)+numRows]])
-                j += 2
-            else:
-                k = i % (numRows-1)
-                newCol = ["_"] * numRows
-                if 2*i+(numRows-2)-((i-(j/2))%(numRows-2)) <= len(s)-1:
-                    newCol[numRows-1-k] = s[2*i+(numRows-2)-((i-(j/2))%(numRows-2))]
-                    matrixOutput.append(newCol)
+        output = [""] * numRows
+        i = 0
+        step = 1
         
-        if not matrixOutput[-1]:
-            matrixOutput.pop()
-        
-        if len(matrixOutput[len(matrixOutput)-1]) < numRows:
-            matrixOutput[len(matrixOutput)-1] += ["_"] * (numRows - len(matrixOutput[len(matrixOutput)-1]))
-        
-        def transpose(matrix):
-            matrixT = [[0 for x in range(len(matrix))] for y in range(len(matrix[0]))]
-            for i in range(len(matrix[0])):
-                for j in range(len(matrix)):
-                    matrixT[i][j] = matrix[j][i]
-            return matrixT
+        for char in s:
+            output[i] += char
+            if i == 0:
+                step = 1
+            if i == numRows - 1:
+                step = -1
+            i += step
             
-        return "".join(["".join(row) for row in transpose(matrixOutput)]).replace("_","")
+        return "".join(output)
