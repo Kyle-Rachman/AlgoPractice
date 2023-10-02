be794b1e45eb7957c63abad89f4dab40c64739b2
Kyle Rachman <127147261+Kyle-Rachman@users.noreply.github.com>
2023-10-02 12:05:11 -0700

    Time: 84 ms (47.13%), Space: 13.7 MB (13.96%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def buildArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return [nums[char] for char in nums]
